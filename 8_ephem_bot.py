f37a94f516dd180d7428a73e7ccb23f6d7a93b37
Andrei Sakovich <sakowlandrei@mail.ru>
2022-09-11 13:41:51 +0200

    'Planet'

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,42 +12,70 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
-import logging
 
+
+import logging
+import ephem
+from datetime import date
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
+
 
 
 def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+    update.message.reply_text('Use command "/planet [name]"')
 
 
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
+def name_planet(update, context):
+    user_text = update.message.text.split()
+    today = date.today().strftime("%Y/%m/%d")
+    planet_name = user_text[1].capitalize()
+  
+    if user_text[0] == '/planet' and len(user_text) < 2:
+        if planet_name == 'Mars':
+            constellationplanet = ephem.constellation(ephem.Mars(today))
+        elif planet_name == 'Jupiter':
+            constellationplanet = ephem.constellation(ephem.Jupiter(today))
+        elif planet_name == 'Saturn':
+            constellationplanet = ephem.constellation(ephem.Saturn(today))
+        elif planet_name == 'Mercury':
+            constellationplanet = ephem.constellation(ephem.Mercury(today))
+        elif planet_name == 'Venus':
+            constellationplanet = ephem.constellation(ephem.Venus(today))
+        elif planet_name == 'Earth':
+            constellationplanet = ephem.constellation(ephem.Earth(today))
+        elif planet_name == 'Uranus':
+            constellationplanet = ephem.constellation(ephem.Uranus(today))
+        elif planet_name == 'Neptune':
+            constellationplanet = ephem.constellation(ephem.Neptune(today))
+        elif planet_name == 'Pluto':
+            constellationplanet = ephem.constellation(ephem.Pluto(today))    
+        else:
+            update.message.reply_text('Unknown planet. Please try again')
+        update.message.reply_text(f'Planete {planet_name} is in {constellationplanet[1]}')
+    else:
+        update.message.reply_text('Unknown command. Please try again')
+        return
+
+
+
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater("5672379591:AAGy1rdE_o0NHnTitJ_J8bqsjKCxeauFS54", use_context=True)
 
     dp = mybot.dispatcher
+
+    dp.start
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(MessageHandler(Filters.text, name_planet))
 
     mybot.start_polling()
     mybot.idle()
